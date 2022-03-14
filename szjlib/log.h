b6945cc612c726593026b52b10e9a49ab3b86c5b
ZhengJie Shi <goodbob256@gmail.com>
2022-03-14 17:10:25 +0800

    szjlib: log: change log function name.

--- szjlib/log.h
+++ szjlib/log.h
@@ -16,17 +16,17 @@ typedef enum
 #define LOG_ERROR(fmt, ...)      do{LOG(LOG_ERROR,   "[E] %s(%u)@%s: "fmt"\n", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__);}while(0)
 
 /* check condition then print log */
-#define LOG_INFO_NO_RETURN(condition, fmt, ...)        do{if(condition)LOG_INFO(fmt, ##__VA_ARGS__);}while(0)
-#define LOG_WARNING_NO_RETURN(condition, fmt, ...)     do{if(condition)LOG_WARNING(fmt, ##__VA_ARGS__);}while(0)
-#define LOG_ERROR_NO_RETURN(condition, fmt, ...)       do{if(condition)LOG_ERROR(fmt, ##__VA_ARGS__);}while(0)
+#define LOG_INFO_CHECK(condition, fmt, ...)        do{if(condition)LOG_INFO(fmt, ##__VA_ARGS__);}while(0)
+#define LOG_WARNING_CHECK(condition, fmt, ...)     do{if(condition)LOG_WARNING(fmt, ##__VA_ARGS__);}while(0)
+#define LOG_ERROR_CHECK(condition, fmt, ...)       do{if(condition)LOG_ERROR(fmt, ##__VA_ARGS__);}while(0)
 
-#define LOG_INFO_RETURN_VOID(condition, fmt, ...)      do{if(condition){LOG_INFO(fmt, ##__VA_ARGS__); return;}}while(0)
-#define LOG_WARNING_RETURN_VOID(condition, fmt, ...)   do{if(condition){LOG_WARNING(fmt, ##__VA_ARGS__); return;}}while(0)
-#define LOG_RETURN_VOID_ERROR(condition, fmt, ...)     do{if(condition){LOG_ERROR(fmt, ##__VA_ARGS__); return;}}while(0)
+#define LOG_INFO_CHECK_RETURN_VOID(condition, fmt,...)      do{if(condition){LOG_INFO(fmt, ##__VA_ARGS__); return;}}while(0)
+#define LOG_WARNING_CHECK_RETURN_VOID(condition, fmt, ...)   do{if(condition){LOG_WARNING(fmt, ##__VA_ARGS__); return;}}while(0)
+#define LOG_ERROR_CHECK_RETURN_VOID(condition, fmt, ...)     do{if(condition){LOG_ERROR(fmt, ##__VA_ARGS__); return;}}while(0)
 
-#define LOG_INFO_RETURN(condition, ret, fmt, ...)      do{if(condition){LOG_INFO(fmt, ##__VA_ARGS__); return ret;}}while(0)
-#define LOG_WARNING_RETURN(condition, ret, fmt, ...)   do{if(condition){LOG_WARNING(fmt, ##__VA_ARGS__); return ret;}}while(0)
-#define LOG_ERROR_RETURN(condition, ret, fmt, ...)     do{if(condition){LOG_ERROR(fmt, ##__VA_ARGS__); return ret;}}while(0)
+#define LOG_INFO_CHECK_RETURN_RET(condition, ret, fmt, ...)      do{if(condition){LOG_INFO(fmt, ##__VA_ARGS__); return ret;}}while(0)
+#define LOG_WARNING_CHECK_RETURN_RET(condition, ret, fmt, ...)   do{if(condition){LOG_WARNING(fmt, ##__VA_ARGS__); return ret;}}while(0)
+#define LOG_ERROR_CHECK_RETURN_RET(condition, ret, fmt, ...)     do{if(condition){LOG_ERROR(fmt, ##__VA_ARGS__); return ret;}}while(0)
 
 void LOG_setLogLevel(LOG_LEVEL_EN enLogLevel);
 void LOG(LOG_LEVEL_EN enLogLevel, const char* fmt, ...);
