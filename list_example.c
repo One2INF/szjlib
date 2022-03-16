9556200912849bd81430386bfbb0cec665cc92de
ZhengJie Shi <goodbob256@gmail.com>
2022-03-16 20:32:01 +0800

    list_example: update list example.

--- list_example.c
+++ list_example.c
@@ -16,22 +16,22 @@ typedef struct
 void func(const NODE_T *node)
 {
   DATA_T *data = (DATA_T *)node->data;
-  LOG_INFO("%4d %32s", data->a, data->str);
+  LOG_INFO("%4d %16s", data->a, data->str);
 }
 
 int main(void)
 {
-  LIST_T stList;
-  List_create(&stList, sizeof(DATA_T));
+  LIST list = NULL;
+  List_create(&list, sizeof(DATA_T));
 
-  LIST_addItem(&stList, stList.head, &(DATA_T){1, "i am 1"});
-  LIST_addItem(&stList, stList.head, &(DATA_T){2, "i am 2"});
-  LIST_addItem(&stList, stList.head, &(DATA_T){3, "i am 3"});
-  LIST_addItem(&stList, stList.head, &(DATA_T){4, "i am 4"});
-  LIST_addItem(&stList, stList.head, &(DATA_T){5, "i am 5"});
+  LIST_addItem(list, list, &(DATA_T){1, "I am 1"});
+  LIST_addItem(list, list, &(DATA_T){2, "I am 2"});
+  LIST_addItem(list, list, &(DATA_T){3, "I am 3"});
+  LIST_addItem(list, list, &(DATA_T){4, "I am 4"});
+  LIST_addItem(list, list, &(DATA_T){5, "I am 5"});
 
-  LIST_travel(&stList, func);
-  LIST_destroy(&stList);
+  LIST_travel(list, func);
+  LIST_destroy(&list);
 
   return 0;
 }
