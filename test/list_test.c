682c1e2ef6a2fddb43af053256a65bc814a5e8b3
ZhengJie Shi <goodbob256@gmail.com>
2022-03-31 10:09:15 +0800

    test: add list test notes, to be realized.

---
+++ test/list_test.c
@@ -0,0 +1,20 @@
+/*
+ * TODO 
+ * list 测试实现
+ * 
+ * LIST_empty: list 是否为空。
+ * LIST_size: list 元素数量。
+ * 
+ * LIST_front: 返回 list 第一个元素。
+ * LIST_back: 返回 list 最后一个元素。
+ * 
+ * LIST_pushFront: 考虑向 空/非空 list 添加。
+ * LIST_pushBack: 考虑向 空/非空 list 添加。
+ * LIST_addItem: 考虑向 空/非空 list 添加。
+ * LIST_popFront: 考虑向 空/非空 list 删除。
+ * LIST_popBack: 考虑向 空/非空 list 删除。
+ * LIST_erase: 考虑向 空/非空 list 删除。
+ * 
+ * LIST_destory：销毁 list，检查是否有内存泄漏。
+ * LIST_clear: 清空 list 所以元素，检查是否有内存泄漏。
+ */
