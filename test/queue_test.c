749116b4bc64a412b344e1a987337a1f11c793e5
ZhengJie Shi <goodbob256@gmail.com>
2022-04-02 13:59:04 +0800

    szjlib: test: add queue test files.

---
+++ test/queue_test.c
@@ -0,0 +1,21 @@
+/*
+ * TODO 
+ * queue 测试实现
+ * 
+ * QUEUE_empty: queue 是否为空。
+ * QUEUE_size: queue 元素数量。
+ * QUEUE_maxSize: queue 容量大小。
+ * 
+ * QUEUE_front: 返回 queue 第一个元素。
+ * QUEUE_back: 返回 queue 最后一个元素。
+ * QUEUE_at: 按下标返回 queue 元素。
+ * 
+ * QUEUE_pushFront: 考虑向 空/非空 queue 添加。
+ * QUEUE_pushBack: 考虑向 空/非空 queue 添加。
+ * QUEUE_popFront: 考虑向 空/非空 queue 删除。
+ * QUEUE_popBack: 考虑向 空/非空 queue 删除。
+ * 
+ * QUEUE_destory：销毁 queue，检查是否有内存泄漏。
+ * QUEUE_traverse: 查看遍历是否正确。
+ * QUEUE_clear: 清空 queue 所以元素，检查是否有内存泄漏。
+ */
