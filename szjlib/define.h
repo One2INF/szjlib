50eb926d51e7c16bcbf68b9ae894dfbe8a040334
ZhengJie Shi <goodbob256@gmail.com>
2022-04-08 10:52:23 +0800

    szjlib: 添加 define 头文件。

---
+++ szjlib/define.h
@@ -0,0 +1,16 @@
+#ifndef SZJLIB_DEFINE_H_
+#define SZJLIB_DEFINE_H_
+
+/* macro define */
+#define IN              const
+#define INOUT
+#define OUT
+
+
+typedef enum
+{
+	SZJLIB_SUCCESSED = 0,
+	SZJLIB_FAILED
+}RET_E;
+
+#endif
